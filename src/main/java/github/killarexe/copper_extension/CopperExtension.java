52ce90c7d28ba0cba7f260528e551357b1b25642
Killarexe <killar.exe@gmail.com>
2023-08-18 22:39:09 +0200

    Setup workspace...

---
+++ src/main/java/github/killarexe/copper_extension/CopperExtension.java
@@ -0,0 +1,15 @@
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CopperExtension.MOD_ID)
+public class CopperExtension{
+	public static final String MOD_ID = "copper_extension";
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public CopperExtension(){
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
