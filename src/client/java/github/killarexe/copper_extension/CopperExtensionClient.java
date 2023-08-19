0f42cb0bc6e7d12476124b76e9fb9bda34e28628
Killarexe <killar.exe@gmail.com>
2023-08-19 09:15:11 +0200

    Terminated setup env

---
+++ src/client/java/github/killarexe/copper_extension/CopperExtensionClient.java
@@ -0,0 +1,9 @@
+package github.killarexe.copper_extension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperExtensionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
--- src/client/java/github/killarexe/copper_extension/mixin/client/ExampleClientMixin.java
+++ src/client/java/github/killarexe/copper_extension/mixin/client/ExampleClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin.client;
+package github.killarexe.copper_extension.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +10,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void run(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
 	}
 }
