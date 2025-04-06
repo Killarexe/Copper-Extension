0f0561f141e94b211d9a667ae2048760fb65b98f
Killar.exe <killar.exe@gmail.com>
2025-04-06 17:00:59 +0200

    Update to Minecraft 1.21.5

--- neoforge/src/main/java/github/killarexe/copper_extension/neoforge/mixin/ItemMixin.java
+++ neoforge/src/main/java/github/killarexe/copper_extension/neoforge/mixin/ItemMixin.java
@@ -5,6 +5,7 @@ import github.killarexe.copper_extension.CEMaps;
 import github.killarexe.copper_extension.neoforge.registry.CEGameRules;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.neoforged.neoforge.common.extensions.IItemExtension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,13 +26,13 @@ import net.minecraft.world.level.Level;
 @Mixin(Item.class)
 public abstract class ItemMixin implements FeatureElement, ItemLike, IItemExtension {
 
-  @Inject(method = "inventoryTick(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/Entity;IZ)V", at = @At("HEAD"))
-  public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean selected, CallbackInfo callbackInfo) {
+  @Inject(method = "inventoryTick(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/EquipmentSlot;)V", at = @At("HEAD"))
+  public void inventoryTick(ItemStack stack, ServerLevel level, Entity entity, EquipmentSlot slot, CallbackInfo ci) {
     Item item = stack.getItem();
-    if(level instanceof ServerLevel serverLevel && entity instanceof Player player && CEMaps.OXIDATION_MAP_ITEMS.containsKey(item)) {
+    if(entity instanceof Player player && CEMaps.OXIDATION_MAP_ITEMS.containsKey(item)) {
       int count = stack.getCount();
-      if(player.getRandom().nextFloat() < serverLevel.getGameRules().getInt(CEGameRules.COPPER_OXIDATION_CHANCE) * CEActions.BASE_CHANCE / count) {
-        player.getInventory().setItem(slot, new ItemStack(CEMaps.OXIDATION_MAP_ITEMS.get(item), count));
+      if(player.getRandom().nextFloat() < level.getGameRules().getInt(CEGameRules.COPPER_OXIDATION_CHANCE) * CEActions.BASE_CHANCE / count) {
+        player.getInventory().setItem(slot.getIndex(), new ItemStack(CEMaps.OXIDATION_MAP_ITEMS.get(item), count));
       }
     }
   }
